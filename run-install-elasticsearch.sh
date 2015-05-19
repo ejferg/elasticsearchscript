472b031e4b270fdd1b03cde94229597d4e8875ec
Earl Ferguson (BlueFletch) <earl.ferguson@bluefletch.com>
2015-05-19 14:50:05 -0400

    Download deb if needed

--- run-install-elasticsearch.sh
+++ run-install-elasticsearch.sh
@@ -1,4 +1,12 @@
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
+#!/bin/bash
+
+es_deb_file="./elasticsearch-0.90.7.deb"
+es_deb_url="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb"
+
+if [ !-f "$es_deb_file" ] then
+  wget $es_deb_url;
+fi
+
 dpkg -i elasticsearch-0.90.7.deb
 rm -r /etc/elasticsearch/elasticsearch.yml
 mv elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
