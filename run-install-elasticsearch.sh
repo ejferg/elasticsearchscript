0518e38c3ab6a5ad911d57e939064d31033856f0
Earl Ferguson (BlueFletch) <earl.ferguson@bluefletch.com>
2015-05-19 14:50:48 -0400

    commit out code

--- run-install-elasticsearch.sh
+++ run-install-elasticsearch.sh
@@ -2,12 +2,13 @@
 
 es_deb_file="./elasticsearch-0.90.7.deb"
 es_deb_url="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb"
+es_yml_file_loc="/etc/elasticsearch/elasticsearch.yml"
 
 if [ !-f "$es_deb_file" ] then
   wget $es_deb_url;
 fi
 
-dpkg -i elasticsearch-0.90.7.deb
-rm -r /etc/elasticsearch/elasticsearch.yml
-mv elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-service elasticsearch restart
+#dpkg -i elasticsearch-0.90.7.deb
+#rm -r $es_yml_file_loc
+#mv elasticsearch.yml $es_yml_file_loc
+#service elasticsearch restart
