dd3243ba676b2e655725d4ced24caf6d572ab9ad
Earl Ferguson (BlueFletch) <earl.ferguson@bluefletch.com>
2015-05-20 11:29:14 -0400

    Update

--- run-install-elasticsearch.sh
+++ run-install-elasticsearch.sh
@@ -3,6 +3,7 @@
 es_deb_file="./elasticsearch-1.5.2.deb"
 es_deb_url="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb"
 es_yml_file_loc="/etc/elasticsearch/elasticsearch.yml"
+es_install_loc=""
 
 if [ ! -f $es_deb_file ]; then
   wget $es_deb_url
@@ -10,10 +11,11 @@ fi
 
 if sudo service --status-all | grep "elasticsearch"; then
     echo "service is installed"
+    #sudo service elasticsearch stop
     #Todo: uninstall elasticsearch
 fi
 
-#dpkg -i $es_deb_file
-#sudo rm -r $es_yml_file_loc
-#sudo mv elasticsearch.yml $es_yml_file_loc
-#sudo service elasticsearch restart
+dpkg -i $es_deb_file
+sudo rm -r $es_yml_file_loc
+sudo mv elasticsearch.yml $es_yml_file_loc
+sudo service elasticsearch restart
