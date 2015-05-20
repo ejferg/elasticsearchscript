5a69f58aae2248825045066ac229229b8db05892
Earl Ferguson (BlueFletch) <earl.ferguson@bluefletch.com>
2015-05-20 10:20:12 -0400

    Release the beast

--- run-install-elasticsearch.sh
+++ run-install-elasticsearch.sh
@@ -4,11 +4,11 @@ es_deb_file="./elasticsearch-1.5.2.deb"
 es_deb_url="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb"
 es_yml_file_loc="/etc/elasticsearch/elasticsearch.yml"
 
-if [ !-f "$es_deb_file" ]; then
+if [ ! -f $es_deb_file ]; then
   wget $es_deb_url
 fi
 
-#dpkg -i elasticsearch-0.90.7.deb
-#rm -r $es_yml_file_loc
-#mv elasticsearch.yml $es_yml_file_loc
-#service elasticsearch restart
+dpkg -i $es_deb_file
+rm -r $es_yml_file_loc
+mv elasticsearch.yml $es_yml_file_loc
+service elasticsearch restart
